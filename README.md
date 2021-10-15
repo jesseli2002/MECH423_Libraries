a87e06501ad7cb562686aacf48e2c4be363c8c5e
Jesse Li <jesse.li2002@gmail.com>
2021-10-14 21:05:49 -0700

    Improved documentation

--- README.md
+++ README.md
@@ -13,3 +13,29 @@
 Note that since most of these are modules, any free functions and global variables should be prefixed with the module name, e.g. `Uart_readData()`.
 
 Suffix any internal variables (i.e. anything that library users shouldn't be touching, the equivalent of private variables in C#/other object oriented languages) with an underscore. E.g. the UART module is probably going to need circular buffers for received data, so an acceptable name for this buffer is `Uart_buffer_`, since code shouldn't interface directly with the buffer; instead using the provided functions.
+
+Have a banner at the top of each component, something like
+```c
+/*
+ * Circular Buffer ===========================================
+ * Circular Buffer ===========================================
+ * Circular Buffer ===========================================
+ */
+```
+so it can be easily spotted if you're searching for it, as well as having something to Ctrl-F for. Later, we can make an index of all of the libraries, complete with Ctrl-F terms.
+
+Document all your functions. Ideally, follow a docgen convention, something like
+```c
+/*
+@brief Description of the method
+@param param_name Description of the argument
+@param param_name2 Description of the argument
+@return Return value, only if applicable
+
+Feel free to add more detailed information at the bottom like so
+*/
+void finagleTheThingamabobs() {
+    // ...
+}
+```
+Also include plenty of comments describing what you're doing. Ideally, the comments will contain enough info that you don't have to go to the datasheet for most activities.
--- common.c
+++ common.c
@@ -16,6 +16,14 @@
  * Circular Buffer ===========================================
  * Circular Buffer ===========================================
  */
+
+/*
+Usage:
+Buffer_Buffer(mybuf, 64); // creates a variable called `mybuf` with buffer size 64
+Buffer_write(&mybuf, 10); // mybuf can be used as a normal variable
+*/
+
+// Struct definition
 typedef struct {
     unsigned char* dat_;
     int size_;
@@ -26,13 +34,12 @@ typedef struct {
     unsigned char varname_##__LINE__##_buf[size]; \
     Buffer varname = {varname_##__LINE__##_buf, size, varname_##__LINE__##_buf, varname_##__LINE__##_buf};
 
-/*
-Usage:
-Buffer_Buffer(mybuf, 64); // creates a variable called `mybuf` with buffer size 64
-Buffer_write(&mybuf, 10); // mybuf can be used as normal
+/**
+ * @brief Writes (enqueues) data into the buffer
+ * @param buf Pointer to buffer
+ * @param val Value to add to buffer
+ * @return True if successful, false if there was overflow (too much data in buffer). In case of overflow, no data is written.
 */
-
-// Tries to write to buffer; returns false if overflow
 bool Buffer_write(Buffer* buf, unsigned char val) {
     volatile unsigned char* wi = buf->wi_;
     *wi++ = val;
@@ -48,9 +55,11 @@ bool Buffer_write(Buffer* buf, unsigned char val) {
     return true;
 }
 
-/*
- * Tries to read from buffer. If no data is present, returns -1
- **/
+/**
+ * @brief Reads (dequeues) data from buffer
+ * @param buf Pointer to buffer
+ * @return Read value if successful, or -1 if no data available.
+*/
 int Buffer_read(Buffer* buf) {
     volatile unsigned char* ri = buf->ri_;
     if (ri == buf->wi_){
@@ -66,8 +75,10 @@ int Buffer_read(Buffer* buf) {
     return result;
 }
 
-/*
- * Returns true if the buffer is empty; false otherwise.
+/**
+ * @brief Checks if the buffer is empty.
+ * @param buf Pointer to buffer
+ * @return True if the buffer is empty; false otherwise.
  **/
 bool Buffer_empty(Buffer* buf) {
     return buf->wi_ == buf->ri_;
@@ -76,6 +87,8 @@ bool Buffer_empty(Buffer* buf) {
 
 /*
  * LED Configuration ============================================
+ * LED Configuration ============================================
+ * LED Configuration ============================================
  */
 inline void Led_init(unsigned char leds);
 inline void Led_enable(unsigned char leds);
@@ -83,18 +96,18 @@ inline void Led_disable(unsigned char leds);
 inline void Led_set(unsigned char leds);
 /*
 LED Output table:
-LED8: P3OUT |= BIT7;
-LED7: P3OUT |= BIT6;
-LED6: P3OUT |= BIT5;
-LED5: P3OUT |= BIT4;
-LED4: PJOUT |= BIT3;
-LED3: PJOUT |= BIT2;
-LED2: PJOUT |= BIT1;
-LED1: PJOUT |= BIT0;
+LED8: P3, BIT7
+LED7: P3, BIT6
+LED6: P3, BIT5
+LED5: P3, BIT4
+LED4: PJ, BIT3
+LED3: PJ, BIT2
+LED2: PJ, BIT1
+LED1: PJ, BIT0
 */
 
-/*
- * @brief Initialize LEDs, by configuring P3DIR and PJDIR.
+/**
+ * @brief Initialize LEDs, by configuring P3DIR and PJDIR. Turns off all LEDs as part of initialization
  * @param leds Bitfield containing which LEDs to initialize. LSB is LED1, MSB is LED8
  **/
 inline void Led_init(unsigned char leds) {
@@ -103,46 +116,35 @@ inline void Led_init(unsigned char leds) {
     Led_disable(0xFF);
 }
 
-/*
+/**
  * @brief Turn on (enable) LEDs
  * @param leds Bitfield containing which LEDs to turn on.
+ *
+ * E.g. Led_enable(BIT0 + BIT4) turns on LED0 and LED4.
  **/
 inline void Led_enable(unsigned char leds) {
     P3OUT |= leds & 0xF0;
     PJOUT |= leds & 0x0F;
 }
 
-/*
+/**
  * @brief Turn off (disable) LEDs
  * @param leds Bitfield containing which LEDs to turn off.
+ *
+ * E.g. Led_disable(BIT0 + BIT4) turns off LED0 and LED4.
  **/
 inline void Led_disable(unsigned char leds) {
     P3OUT &= (~leds) & 0xF0;
     PJOUT &= (~leds) & 0x0F;
 }
 
-/*
+/**
  * @brief Set state of LEDs,
  * @param leds Bitfield containing which LEDs to turn on. Remaining bits will be turned off.
+ *
+ * E.g. Led_set(BIT0 + BIT4) turns on LED0 and LED4, and turns off all other LEDs.
  **/
 inline void Led_set(unsigned char leds) {
     Led_disable(~leds);
     Led_enable(leds);
-}
-
-
-
-
-
-
-
-
-/**
- * main.c
- */
-int main(void)
-{
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-
-	return 0;
 }
