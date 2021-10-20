02980b77ce96195031af4040e8b039a3904c843f
Jesse Li <jesse.li2002@gmail.com>
2021-10-19 20:57:57 -0700

    Variable clock freq and baud rate

--- UART.h
+++ UART.h
@@ -50,10 +50,136 @@ void Uart_begin(unsigned int clockSource)
     UCA0IE |= UCRXIE;                    //Enable the transmit interrupt
 }
 
+float Uart_baudLookupIn_[] = {
+    0.0000,
+    0.0529,
+    0.0715,
+    0.0835,
+    0.1001,
+    0.1252,
+    0.1430,
+    0.1670,
+    0.2147,
+    0.2224,
+    0.2503,
+    0.3000,
+    0.3335,
+    0.3575,
+    0.3753,
+    0.4003,
+    0.4286,
+    0.4378,
+    0.5002,
+    0.5715,
+    0.6003,
+    0.6254,
+    0.6432,
+    0.6667,
+    0.7001,
+    0.7147,
+    0.7503,
+    0.7861,
+    0.8004,
+    0.8333,
+    0.8464,
+    0.8572,
+    0.8751,
+    0.9004,
+    0.9170,
+    0.9288
+}
+unsigned int Uart_baudLookupOut_[] = {
+    0x00,
+    0x01,
+    0x02,
+    0x04,
+    0x08,
+    0x10,
+    0x20,
+    0x11,
+    0x21,
+    0x22,
+    0x44,
+    0x25,
+    0x49,
+    0x4A,
+    0x52,
+    0x92,
+    0x53,
+    0x55,
+    0xAA,
+    0x6B,
+    0xAD,
+    0xB5,
+    0xB6,
+    0xD6,
+    0xB7,
+    0xBB,
+    0xDD,
+    0xED,
+    0xEE,
+    0xBF,
+    0xDF,
+    0xEF,
+    0xF7,
+    0xFB,
+    0xFD,
+    0xFE,
+}
+
+
+
+/**
+ * @brief Sets up the UART at a non-default baud rate for a different clock frequency.
+ * @param clockSource One of UCSSEL__UCLK, UCSSEL__ACLK, UCSSEL__SMCLK. Defines the source clock for the UART.
+ * @param freq Frequency of the clock source, in Hz
+ * @param baud Baud rate. 9600 is most common.
+*/
+void Uart_beginAtFreq(unsigned int clockSource, unsigned long freq, unsigned long baud){
+    // Configure ports for UCA0 [ports for UCA0 TX and RX]
+    P2SEL0 &= ~(BIT0 + BIT1);
+    P2SEL1 |= BIT0 + BIT1;
+
+    // Enable UC software reset while modifying settings
+    UCA0CTLW0 |= UCSWRST;
+
+    // Determine baud rate parameters
+    float N = freq / baud;
+    unsigned int os16, ucbrx, ucbrfx, ucbrsx = 0;
+    if (N > 16){
+        os16 = 1;
+        float N_div16 = N / 16;
+
+        ucbrx = (int) N_div16;
+        ucbrfx = (int)((N_div16 - (int)(N_div16)) * 16)
+    } else {
+        os16 = 0;
+        ucbrx = (int) N;
+        ucbrfx = 0;
+    }
+
+    int i;
+    float N_frac = N - (int)N;
+    for (i = 0; i < 36; ++i){
+        if(N_frac >= Uart_baudLookupIn_[i]) {
+            ucbrsx = Uart_baudLookupOut_[i];
+            break;
+        }
+    }
+
+    UCA0BRW = ucbrx; // UCBRx
+    UCA0MCTLW_L = (ucbrfx /* = UCBRFx */ << 4) + os16 /* OS16 = 1*/;
+    UCA0MCTLW_H = ucbrsx; // UCBRSx
+    UCA0CTLW0 = clockSource;
+
+    // Clear UC software reset to enable UART
+    UCA0CTLW0 &= ~UCSWRST;
+}
+
  /**
   * This function writes a single character to the UART
   * @param single character to be written to the UART
-  * @param return void
+  * @return void
   */
 
 void Uart_write(char CharacterToWriteToUart)
